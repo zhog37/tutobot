42ad1ae90f0c437ffd1de8fb237c3ec58956085f
XeCrafT <32407457+XeCrafT@users.noreply.github.com>
2017-11-11 09:54:06 -0500

    Create index.js

---
+++ .gitignore/index.js
@@ -0,0 +1,10 @@
+const Discord = require("discord.js");
+
+var bot = new Discord.Client();
+
+bot.on("ready", function() {
+    bot.user.setGame("TutoBot, !help");
+    console.log("Le bot a bien ete connecte");
+});
+
+bot.login("Mzc4OTE0OTg3OTk0Nzc1NTYy.DOibng.yzNvWznRQbUbcoN_qSIWzbeK3qs");
