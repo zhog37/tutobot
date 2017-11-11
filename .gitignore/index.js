501dd45e8b2983ee4ed43276d378fa48f165465f
XeCrafT <32407457+XeCrafT@users.noreply.github.com>
2017-11-11 10:31:19 -0500

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -2,6 +2,8 @@ const Discord = require("discord.js");
 
 var bot = new Discord.Client();
 
+var PREFIX = "!";
+
 bot.on("ready", function() {
     bot.user.setGame("TutoBot, !help");
     console.log("Le bot a bien ete connecte");
@@ -17,6 +19,28 @@ bot.on("message", function(message) {
    if(message.content === "ping") {
        message.channel.sendMessage(randomMessage[Math.floor(Math.random() * randomMessage.length)]);
    }  
+});
+
+bot.on("message", function(message) {
+    if(message.author.equals(bot.user)) return;
+    if(!message.content.substring(PREFIX.length).split(" "));
+
+    var args = message.content.split(" ").slice(1);
+
+    switch (args[0].toLowerCase()) {
+        case "help":
+        var embed = new Discord.RichEmbed()
+        .addField("!help", "Cette commande permet de savoir toute les commandes du bot.")
+        .setColor("#FE2E2E")
+        .setFooter(message.author.username, " Merci d'utiliser le bot ! :D")
+        .setAuthor(message.author.avatarURL)
+        message.delete()
+        message.channel.sendEmbed(embed);
+        break;
+
+        default:
+        message.reply("Cette commande n'existe pas!")
+    }
 });
 
 
