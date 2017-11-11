b71beb4bc844c804e1b3c03f9a93cde2b135abfd
XeCrafT <32407457+XeCrafT@users.noreply.github.com>
2017-11-11 10:05:52 -0500

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -7,4 +7,14 @@ bot.on("ready", function() {
     console.log("Le bot a bien ete connecte");
 });
 
+bot.on("message", function(message) {
+   if(message.content === "ping") {
+       message.reply("Pong!");
+   } 
+   if(message.content === "Bonjour") {
+    message.reply("Coucou!");
+} 
+});
+
+
 bot.login("Mzc4OTE0OTg3OTk0Nzc1NTYy.DOibng.yzNvWznRQbUbcoN_qSIWzbeK3qs");
