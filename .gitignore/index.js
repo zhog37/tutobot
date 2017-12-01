98224359b11a850e1017d1110b73c672ba9ff4da
XeCrafT <32407457+XeCrafT@users.noreply.github.com>
2017-12-01 13:28:54 -0500

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -2,6 +2,8 @@ const Discord = require("discord.js");
 
 var bot = new Discord.Client();
 
+const PREFIX = "!";
+
 bot.on("ready", function() {
     bot.user.setGame("TutoBot, !help");
     console.log("Le bot a bien ete connecte");
@@ -10,10 +12,29 @@ bot.on("ready", function() {
 bot.on('message', function(message) {
     
     if(message.content === 'ping') {
-     message.reply('Pong ! ' + bot.ping + ' !')  
+     message.reply('Pong ! ' + bot.ping + ' ms !')  
     }
     
 });
+
+bot.on('message', async function(message) {
+    if(message.author.equals(bot.user)) return;
+    
+    if(!message.content.startsWith(PREFIX)) return;
+    
+    var args = message.content.substring(PREFIX.length).split (" ");
+    
+    switch (args[0].toLowerCase()) {
+            
+        case "ping":
+           message.reply('Pong ! ' + bot.ping + ' ms !');
+            break;
+            
+            
+        default:
+            message.reply("Commande invalide, faites !help :D");
+    }
+}
     
 
 bot.login(process.env.TOKEN);
