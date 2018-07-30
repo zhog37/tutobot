2ad0a04ddf85bbe6768d8046e43f467516d333bb
Anthony H <32407457+xecraft@users.noreply.github.com>
2018-07-30 09:22:49 -0400

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -2,8 +2,6 @@ const Discord = require("discord.js");
 
 var bot = new Discord.Client();
 
-const PREFIX = "!";
-
 bot.on("ready", function() {
     bot.user.setGame("TutoBot, !help");
     console.log("Le bot a bien ete connecte");
