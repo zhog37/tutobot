441a10cc7a1895caab4a81a21ed92fe7390b3bb6
Anthony H <32407457+xecraft@users.noreply.github.com>
2018-07-30 09:22:41 -0400

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -9,32 +9,4 @@ bot.on("ready", function() {
     console.log("Le bot a bien ete connecte");
 });
 
-bot.on('message', function(message) {
-    
-    if(message.content === 'ping') {
-     message.reply('Pong ! ' + bot.ping + ' ms !')  
-    }
-    
-});
-
-bot.on('message', async function(message) {
-    if(message.author.equals(bot.user)) return;
-    
-    if(!message.content.startsWith(PREFIX)) return;
-    
-    var args = message.content.substring(PREFIX.length).split (" ");
-    
-    switch (args[0].toLowerCase()) {
-            
-        case "ping":
-           message.reply('Pong ! ' + bot.ping + ' ms !');
-            break;
-            
-            
-        default:
-            message.reply("Commande invalide, faites !help :D");
-    }
-});
-    
-
 bot.login(process.env.TOKEN);
